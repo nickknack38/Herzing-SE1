7332a8b15e35ade26873a2335a1498c803ffb483
Nick Gibliant <116523698+nickknack38@users.noreply.github.com>
2025-03-15 18:38:10 -0500

    Update IAdminRepo.java

--- IAdminRepo.java
+++ IAdminRepo.java
@@ -1,9 +1,10 @@
 package RestaurantManagementSystem.SpringBootExample.Repository;
 
-
 import RestaurantManagementSystem.SpringBootExample.Model.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IAdminRepo extends JpaRepository<Admin, Integer> {
-    Admin findFirstByAdminEmail(String email);
+    Admin findFirstByAdminEmail(String adminEmail);
 }
